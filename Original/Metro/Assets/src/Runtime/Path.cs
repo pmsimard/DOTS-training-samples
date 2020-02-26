81f5c449b5ad391d67b2cfda204ecfd9e0fa0b7a
pmsimard <pierrem.simard@gmail.com>
2020-02-26 13:19:15 -0500

    path sample

---
+++ Original/Metro/Assets/src/Runtime/Path.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Collections;
+using Unity.Mathematics;
+
+public static class Path
+{
+    public static void GetNextTarget(int currentIndex, float speed, float3 currentPosition, NativeArray<float3> path, out int targetIndex, out float3 targetPosition)
+    {
+        targetIndex = currentIndex;
+        targetPosition = currentPosition;
+
+        float distance = 0f;
+        while (distance < speed)
+        {
+            targetIndex = (targetIndex + 1) % path.Length;
+            targetPosition = path[targetIndex];
+            distance = math.distance(targetPosition, currentPosition);
+        }
+    }
+}
