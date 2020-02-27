6a9b9e6abfd41958597cab9d6b938999c2153f77
jfgoulet <jf.goulet.home@gmail.com>
2020-02-27 11:22:29 -0500

    Track conversion code
    Proper track conversion code

--- Original/Metro/Assets/src/Runtime/MetroLineData.cs
+++ Original/Metro/Assets/src/Runtime/MetroLineData.cs
@@ -1,7 +1,9 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
 
-[InternalBufferCapacity(1024)]
+// Zero size internal buffer array to immediately allocate in the RAM
+// as our track data is fairly large
+[InternalBufferCapacity(0)]
 public struct MetroLinePositionElement : IBufferElementData
 {
     // Actual value each buffer element will store.
@@ -19,7 +21,9 @@ public struct MetroLinePositionElement : IBufferElementData
     }
 }
 
-[InternalBufferCapacity(1024)]
+// Zero size internal buffer array to immediately allocate in the RAM
+// as our track data is fairly large
+[InternalBufferCapacity(0)]
 public struct MetroLineNormalElement : IBufferElementData
 {
     // Actual value each buffer element will store.
